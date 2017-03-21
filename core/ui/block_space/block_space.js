bddeb7647a70f564fb3b403ced2a312345e0ce11
Josh Lory <josh.lory@code.org>
2017-03-21 16:51:46 -0700

    Increase z-index of dragging canvas

--- core/ui/block_space/block_space.js
+++ core/ui/block_space/block_space.js
@@ -337,7 +337,7 @@ Blockly.BlockSpace.prototype.createDom = function() {
       id: 'blocklyDragCanvas',
       width: '100%',
       height: '100%',
-      style: 'pointer-events: none; position: absolute; top: 0; left: 0;'
+      style: 'pointer-events: none; position: absolute; top: 0; left: 0; z-index: 10;'
     }, document.body);
   }
   this.svgDragCanvas_ = Blockly.createSvgElement('g', {'class': 'svgDragCanvas'}, Blockly.dragSvg);

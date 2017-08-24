4ad6d92b060dd4b1ac4a406a384acd581cec48c9
Elijah Hamovitz <elijahhamovitz@gmail.com>
2017-08-24 15:01:23 -0700

    update FieldAngleTextInput to use the angle helper functionality that is now native to TextInput

--- core/ui/fields/field_angle_textinput.js
+++ core/ui/fields/field_angle_textinput.js
@@ -1,70 +1,35 @@
 /**
- * @fileoverview Text input field.
- * @author fraser@google.com (Neil Fraser)
+ * @fileoverview special Text Input field that always shows angle helper
+ * @author elijah@code.org (Elijah Hamovitz)
  */
 'use strict';
 
 goog.provide('Blockly.FieldAngleTextInput');
-goog.require('Blockly.FieldTextInput');
+
 goog.require('Blockly.AngleHelper');
+goog.require('Blockly.BlockFieldHelper');
+goog.require('Blockly.FieldTextInput');
 
 /**
- * Class for an editable text field.
+ * Class for an editable text field which will always show the Angle HElper
+ * @param {string} directionTitle the title of the field from which to obtain
+ *        direction information
  * @param {string} text The initial content of the field.
- * @param {Function} opt_changeHandler An optional function that is called
- *     to validate any constraints on what the user entered.  Takes the new
- *     text as an argument and returns the accepted text or null to abort
- *     the change.
- * @extends {Blockly.Field}
+ * @extends {Blockly.FieldTextInput}
  * @constructor
  */
-Blockly.FieldAngleTextInput = function(directionTitleName, text) {
-  this.angleHelper = null;
-  this.directionTitleName = directionTitleName;
+Blockly.FieldAngleTextInput = function(directionTitle, text) {
+  this.directionTitle = directionTitle;
   Blockly.FieldAngleTextInput.superClass_.constructor.call(this,
       text, Blockly.FieldTextInput.numberValidator);
 };
 goog.inherits(Blockly.FieldAngleTextInput, Blockly.FieldTextInput);
 
-Blockly.FieldAngleTextInput.SIZE = 150;
-
-Blockly.FieldAngleTextInput.prototype.dispose = function() {
-  if (this.angleHelper) {
-    this.angleHelper.dispose();
+Blockly.FieldAngleTextInput.prototype.getFieldHelperOptions_ = function(field_helper) {
+  if (field_helper === Blockly.BlockFieldHelper.ANGLE_HELPER) {
+      return {
+        directionTitle: this.directionTitle,
+        block: this.sourceBlock_
+      }
   }
-  Blockly.FieldAngleTextInput.superClass_.dispose.call(this);
-};
-
-/**
- * Show the inline free-text editor on top of the text.
- * @private
- */
-Blockly.FieldAngleTextInput.prototype.showEditor_ = function() {
-  Blockly.FieldAngleTextInput.superClass_.showEditor_.call(this);
-  var div = Blockly.WidgetDiv.DIV;
-
-  var container = goog.dom.createDom('div', 'blocklyFieldAngleTextInput');
-  container.style.height = Blockly.FieldAngleTextInput.SIZE + 'px';
-  container.style.width = Blockly.FieldAngleTextInput.SIZE + 'px';
-  div.appendChild(container);
-
-  var dir = this.sourceBlock_.getTitleValue(this.directionTitleName);
-  this.angleHelper = new Blockly.AngleHelper(dir, {
-    onUpdate: function () {
-      var value = this.angleHelper.getAngle().toString();
-      this.setText(value);
-      Blockly.FieldTextInput.htmlInput_.value = value;
-    }.bind(this),
-    arcColour: this.sourceBlock_.getHexColour(),
-    height: Blockly.FieldAngleTextInput.SIZE,
-    width: Blockly.FieldAngleTextInput.SIZE,
-    angle: parseInt(this.getValue())
-  });
-
-  this.angleHelper.init(container);
-};
-
-Blockly.FieldAngleTextInput.prototype.onHtmlInputChange_ = function(e) {
-  Blockly.FieldAngleTextInput.superClass_.onHtmlInputChange_.call(this, e);
-  this.angleHelper.animateAngleChange(parseInt(this.getText()));
 };

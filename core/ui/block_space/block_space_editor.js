95894c7f3d4b5854fa36b0ebec9f3d87cd96d034
Josh Lory <josh.lory@code.org>
2018-06-19 17:04:02 -0700

    Allow pasting blocks from other applications

--- core/ui/block_space/block_space_editor.js
+++ core/ui/block_space/block_space_editor.js
@@ -761,7 +761,7 @@ Blockly.BlockSpaceEditor.prototype.onCutCopy_ = function(e) {
  */
 Blockly.BlockSpaceEditor.prototype.onPaste_ = function(e) {
   try {
-    var xml = Blockly.Xml.textToDom(e.clipboardData.getData('text/xml'));
+    var xml = Blockly.Xml.textToDom(e.clipboardData.getData('text/xml') || e.clipboardData.getData('text/plain'));
     Blockly.focusedBlockSpace.paste({
       dom: xml.firstElementChild
     });

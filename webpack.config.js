cf935e3482964ec98b11338f8ce18b40ec5a2964
Ram Kandasamy <ram.kandasamy@gmail.com>
2018-07-10 09:53:34 -0700

    Respect DEV env var in webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,34 +1,38 @@
 const path = require('path');
 const ClosureCompilerPlugin = require('webpack-closure-compiler');
 
+const DEV = process.env.DEV;
+
+const compilerOptions = {
+  jar: 'node_modules/google-closure-compiler/compiler.jar',
+  compilation_level: DEV ? 'WHITESPACE_ONLY' : 'SIMPLE',
+  entry_point: 'BlocklyModule',
+  only_closure_dependencies: true,
+  create_source_map: true,
+  source_map_include_content: true,
+  js: [
+    'core/',
+    'blocks/',
+    'generators/',
+    'node_modules/google-closure-library/closure'
+  ],
+};
+if (DEV) {
+  compilerOptions.formatting = 'PRETTY_PRINT';
+}
+
 module.exports = {
   name: 'blockly',
-  mode: 'development',
+  mode: DEV ? 'development' : 'production',
   devtool: 'source-map',
   entry: {
     blockly: path.join(__dirname, 'core/module.js'),
   },
   output: {
-    path: path.resolve(__dirname, 'dist'),
+    path: path.join(__dirname, 'dist'),
     filename: 'blockly.js',
   },
   plugins: [
-    new ClosureCompilerPlugin({
-      compiler: {
-        jar: 'node_modules/google-closure-compiler/compiler.jar',
-        compilation_level: 'SIMPLE',
-        entry_point: 'BlocklyModule',
-        formatting: 'PRETTY_PRINT',
-        only_closure_dependencies: true,
-        create_source_map: true,
-        source_map_include_content: true,
-        js: [
-          'core/',
-          'blocks/',
-          'generators/',
-          'node_modules/google-closure-library/closure'
-        ],
-      },
-    }),
+    new ClosureCompilerPlugin({ compiler: compilerOptions }),
   ],
 };

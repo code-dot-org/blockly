27b38393e60647888d4e12d370d01718d1030e73
Ram Kandasamy <ram.kandasamy@gmail.com>
2018-07-12 11:08:29 -0700

    Remove default values from karma.conf.js

--- karma.conf.js
+++ karma.conf.js
@@ -2,17 +2,8 @@
 module.exports = function(config) {
   const coverage = !!process.env.COVERAGE;
   config.set({
-
-    // base path that will be used to resolve all patterns (eg. files, exclude)
-    basePath: '',
-
-
-    // frameworks to use
-    // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
     frameworks: ['custom'],
 
-
-    // list of files / patterns to load in the browser
     files: [
       'node_modules/google-closure-library/closure/goog/base.js',
       'tests/test_dependency_map.js',
@@ -25,25 +16,16 @@ module.exports = function(config) {
       { pattern: 'generators/**/*.js', included: false },
     ],
 
-
-    // list of files / patterns to exclude
     exclude: [
       'tests/playground_requires.js',
     ],
 
-
-    // preprocess matching files before serving them to the browser
-    // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: coverage ? {
       'core/**/*.js': ['coverage'],
       'blocks/**/*.js': ['coverage'],
       'generators/**/*.js': ['coverage'],
     } : {},
 
-
-    // test results reporter to use
-    // possible values: 'dots', 'progress'
-    // available reporters: https://npmjs.org/browse/keyword/karma-reporter
     reporters: coverage ? ['progress', 'coverage'] : ['progress'],
 
     coverageReporter: {
@@ -53,36 +35,8 @@ module.exports = function(config) {
       ],
     },
 
-
-    // web server port
-    port: 9876,
-
-
-    // enable / disable colors in the output (reporters and logs)
-    colors: true,
-
-
-    // level of logging
-    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
     logLevel: config.LOG_WARN,
-
-
-    // enable / disable watching file and executing tests whenever any file changes
-    autoWatch: true,
-
-
-    // start these browsers
     browsers: ['ChromeHeadless'],
-
-
-    // Continuous Integration mode
-    // if true, Karma captures browsers, runs the tests and exits
-    singleRun: false,
-
-    // Concurrency level
-    // how many browser should be started simultaneous
-    concurrency: Infinity,
-
     client: {
       captureConsole: false,
     },

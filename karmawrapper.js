2fc8f14a385c8ca154fc4e45bd2fe91a4fe356cf
Ram Kandasamy <ram.kandasamy@gmail.com>
2018-07-11 17:52:59 -0700

    Use karma to run blockly unit tests

---
+++ karmawrapper.js
@@ -0,0 +1,31 @@
+goog.require('Blockly.Test');
+goog.require('goog.testing.jsunit');
+
+let karma;
+function setUpPage() {
+  karma.info({ total: G_testRunner.testCase.getCount() });
+}
+
+let numErrors = 0;
+function tearDown() {
+  const errors = G_testRunner.testCase.result_.errors;
+  const success = errors.length === numErrors;
+  numErrors = errors.length;
+  const lastError = errors[errors.length - 1];
+  karma.result({
+    description: G_testRunner.testCase.curTest_.name,
+    success,
+    suite: ['Blockly'],
+    log: success ? [] : [lastError.message, ...lastError.stack.split('\n').slice(0, 3)],
+  });
+}
+
+function tearDownPage() {
+  karma.complete({});
+}
+
+window.karmaCustomEnv = {
+  execute: function (k) {
+    karma = k;
+  },
+};

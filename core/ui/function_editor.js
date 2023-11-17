cf5c751b39f97be68a4b47a0e436fe5a03f8b938
Emily Eastlake <emily.eastlake@code.org>
2023-11-16 22:58:51 -0800

    fix prettier error

--- core/ui/function_editor.js
+++ core/ui/function_editor.js
@@ -782,7 +782,10 @@ Blockly.FunctionEditor.prototype.create_ = function() {
       this.container_.querySelector('#functionNameText').value = value;
     }
     this.functionDefinitionBlock.setTitleValue(value, 'NAME');
-    if (this.functionDefinitionBlock.userCreated && this.functionDefinitionBlock.type === 'behavior_definition') {
+    if (
+      this.functionDefinitionBlock.userCreated &&
+      this.functionDefinitionBlock.type === 'behavior_definition'
+    ) {
       this.functionDefinitionBlock.getTitle_('NAME').id = value;
     }
     if (Blockly.Blocks.gamelab_behaviorPicker) {
